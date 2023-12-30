de5be2d5db3c40afbd3a89606f7fd86f4b837b07
aum-radia <88178583+aum-radia@users.noreply.github.com>
2023-12-30 10:13:53 +0000

    datatypes in js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //Treat all JS Code as newer version
+
+// alert(3+3) //We are using nodejs, not browser
+
+// code readability is must
+
+let name = "Aum Radia"
+let age = 23
+let isLoggedIn = true
+
+// Number => 2 to power 53
+// Big Int
+// String => " "
+// Boolean => true / false
+// null => stabdlone value Empty value // null is object type
+// undefined => value not assigned // it's type is undefined
+// symbol => for uniqueness
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
