fd870a84c4ceb8c279fdd52c5c71d8e7c5fa1659
aum-radia <88178583+aum-radia@users.noreply.github.com>
2023-12-30 11:17:32 +0000

    operators

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,26 +1,55 @@
-let score = "12a" //Number
+// let score = "12a" //Number
 
-console.log(typeof (score));
-console.log(typeof score);
+// console.log(typeof (score));
+// console.log(typeof score);
 
-let valueNumber = Number(score)
+// let valueNumber = Number(score)
 
-console.log(typeof valueNumber);
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; fa;se => 0
 
-let isLoggedIn = "aum"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "aum"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // " " => false
 // "Aum" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+
+// ************* Operations **************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+// let str1 = "Hello"
+// let str2 = " Aum"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2) //12
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") //32
+
+// console.log((3+4) * 5 % 3)
+// console.log(+true); //1
+// //console.log(true+); //error
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3= 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
