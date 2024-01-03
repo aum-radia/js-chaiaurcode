6f058f9cdb2ea3def6394531422aa56df34445c0
aum-radia <88178583+aum-radia@users.noreply.github.com>
2024-01-03 16:37:21 +0000

    Object part 3

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,20 @@
+// Destructuring objects
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+// {
+//     "name": "Aum",
+//     "coursename": "JS Hindi",
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {}
+// ]
+
