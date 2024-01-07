89804ca188cf968cbe9b8c5768c70456999acf90
aum-radia <88178583+aum-radia@users.noreply.github.com>
2024-01-07 17:19:21 +0000

    Scopes part 3

---
+++ 03_basics/05_scopes.js
@@ -0,0 +1,13 @@
+// getName();
+//  console.log(x);
+console.log(getName);
+var x = 7
+
+function getName() {
+    console.log("Aum Radia");
+}
+
+
+// 2 Golden Rules:
+// 1. Variable declarations are scanned and are made undefined
+// 2. Function declarations are scanned and are made available
