e7cb21069a86d6af33c2290d6cd2daf69f1fce1b
aum-radia <88178583+aum-radia@users.noreply.github.com>
2024-01-24 17:27:06 +0000

    For Loop, While Loop and Do While

---
+++ 05_iterations/one.js
@@ -0,0 +1,45 @@
+// For Loop
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is best number");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 0; i <=20; i++) {
+//     console.log(`Outer Loop Value: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         //console.log(`Inner Loop Value: ${j} and inner loop ${i}`);
+//         console.log(i + ' * ' + j + ' = ' + i*j);
+//     }  
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// Break and Continue
+
+// Break
+// for (let index = 0; index <=20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
+
+//continue
+
+// for (let index = 0; index <=20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
